b8c4535a941bd877f8df21ea369d81be7f02d857
browings <hifibasty@t-online.de>
2024-04-23 21:49:39 +0200

    declare basic function populate screen, add querrySelector to all buttons add Eventlistener to buttons called function based on ButtonName

--- script.js
+++ script.js
@@ -34,6 +34,11 @@ function operate(){
     if (operand == "/"){divide(number1, number2)}
 }
 
+function populateDisplay(value){
+display = display+value;
+return display
+}
+
 let number1 = 4;
 
 let number2 = 2;
@@ -41,6 +46,33 @@ let number2 = 2;
 let operand = "/";
 
 let solution;
+
+let display = "";
+
+//querySelector to all buttons
+
+const buttons = document.querySelectorAll(".digit, .digit0, .operand, .operandPlus, .equals, .clear")
+
+//addEventlistener click
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.className === "digit" || "digit0" || "operand" || "operandPlus"){populateDisplay(button.textContent)}
+
+      if (button.className === "equals"){/*evaluate numbers and operands*/ console.log("=")}
+
+      if (button.className === "clear"){clearScreen(); console.log("clear")}
+
+      else;
+    });
+  });
+
+//function value --> populateDisplay() or C function --> clearScreen()
+
+console.log(display);
+
+
+
 
 //****Test */
 
@@ -52,8 +84,8 @@ let solution;
 
 //divide(number1, number2);
 
-operate(number1, number2, operand);
+//operate(number1, number2, operand);
 
 
-console.log(solution)
+//console.log(solution)
 
